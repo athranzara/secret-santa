5b048b9ff4dafb3e7be8b122e2276027d91dd2d2
jingyun <jingyun.zhang377@gmail.com>
2016-12-02 12:39:24 +0000

    utf-8 xml

--- secret_santa.py
+++ secret_santa.py
@@ -177,7 +177,7 @@ call with the --send argument:
                 santee=pair.receiver.name,
             )
             if send:
-                result = server.sendmail(frm, [to], body)
+                result = server.sendmail(frm, [to], body.encode('utf-8'))
                 print "Emailed %s <%s>" % (pair.giver.name, to)
 
         if send:
